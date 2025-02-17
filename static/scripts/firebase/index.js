8e268b39b92566297785b8a4430abd3de6e19937
Dundee Zhang <60833894+dundeezhang@users.noreply.github.com>
2025-02-17 14:26:53 -0500

    Update index.js

--- static/scripts/firebase/index.js
+++ static/scripts/firebase/index.js
@@ -10,13 +10,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAu_fXf_EoNpp10WRs7xc2F3fKbla7WAS0",
-    authDomain: "simple-firebase-91658.firebaseapp.com",
-    projectId: "simple-firebase-91658",
-    storageBucket: "simple-firebase-91658.appspot.com",
-    messagingSenderId: "424748845206",
-    appId: "1:424748845206:web:966c5ed3fc459277411ab7",
-    measurementId: "G-H4WKK504Y4",
+
 };
 
 // Initialize Firebase
